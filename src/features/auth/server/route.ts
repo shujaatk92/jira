3a851e7c3e6e14a80d7fb5468aacd03640387ea9
Shujaat Khan <148489139+shujaatkhanjadoon@users.noreply.github.com>
2024-10-18 12:17:15 +0000

    2:44

--- src/features/auth/server/route.ts
+++ src/features/auth/server/route.ts
@@ -1,6 +1,10 @@
 import { Hono } from "hono";
 import { zValidator } from "@hono/zod-validator";
 import { loginSchema, registerSchema } from "../schemas";
+import { createAdminClient } from "@/lib/appwrite";
+import { ID } from "node-appwrite";
+import { setCookie } from "hono/cookie";
+import { AUTH_COOKIE } from "../constants";
 
 const app = new Hono()
     .post("/login",
@@ -13,10 +17,31 @@ const app = new Hono()
         })
         .post("/register", zValidator("json",
             registerSchema),
-        (c) => {
+        async (c) => {
             const { name, email, password } = c.req.valid("json");
-            console.log({name, email, password});
-            return c.json({name, email, password});
+
+            const { account } = await createAdminClient();
+            const user = await account.create(
+                ID.unique(),
+                email,
+                password,
+                name,
+            );
+
+            const session = await account.createEmailPasswordSession(
+                email,
+                password,
+            );
+            setCookie(c, AUTH_COOKIE, session.secret), {
+                path: "/",
+                httpOnly: true,
+                secure: true,
+                sameSite: "strict",
+                maxAge: 60 * 60 * 24 * 30,
+            }
+
+            return c.json({data: user});
+
         })
 
 export default app;
