65fa2ee4e12e0974b18bdbaa190397ff6c4a3266
Shujaat Khan <148489139+shujaatkhanjadoon@users.noreply.github.com>
2024-10-22 08:02:41 +0000

    4:50

--- src/features/workspaces/schemas.ts
+++ src/features/workspaces/schemas.ts
@@ -2,4 +2,9 @@ import { z } from "zod";
 
 export const createWorkspacesSchema = z.object({
     name: z.string().trim().min(1,"Required"), 
+    image: z.union([
+        z.instanceof(File),
+        z.string().transform((value) => value === "" ? undefined : value),
+    ])
+    .optional(),
 })
