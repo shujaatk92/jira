c754df8cb6daafe914ac91fd68e6b3c1eddc63ee
Shujaat Khan <148489139+shujaatkhanjadoon@users.noreply.github.com>
2024-10-23 12:09:58 +0000

    5:41

--- src/components/workspace-switcher.tsx
+++ src/components/workspace-switcher.tsx
@@ -11,16 +11,23 @@ import {
  
 import { useGetWorkspaces } from "@/features/workspaces/api/use-get-workspaces";
 import { WorkspaceAvatar } from "@/features/workspaces/components/workspace-avatar";
+import { useRouter } from "next/navigation";
 
 export const WorkspaceSwitcher = () => {
+    const router = useRouter();
     const { data: workspaces } = useGetWorkspaces();
+
+    const onSelect = (id: string) => {
+        router.push(`/workspaces/${id}`);
+    } 
+    
     return (
         <div className="flex flex-col gap-y-2">
             <div className="flex items-center justify-between">
                 <p className="text-xs uppercase text-neutral-500">Workspaces</p>
                 <RiAddCircleFill className="size-5 text-neutral-500 hover:opacity-75 transition cursor-pointer"/>
             </div>
-            <Select>
+            <Select onValueChange={onSelect}>
                 <SelectTrigger className="w-full p-1 bg-neutral-100 font-medium">
                     <SelectValue placeholder="No workspace selected" />
                 </SelectTrigger>
