5f75dd5948f247f8269d3fbb9d52ef73850d1f60
Shujaat Khan <148489139+shujaatkhanjadoon@users.noreply.github.com>
2024-10-30 07:36:47 +0000

    7:35:0

---
+++ src/features/workspaces/api/use-delete-workspace.ts
@@ -0,0 +1,33 @@
+import { toast } from "sonner";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { InferRequestType, InferResponseType } from "hono";
+
+import { client } from "@/lib/rpc";
+
+type ResponseType = InferResponseType<typeof client.api.workspaces[":workspaceId"]["$delete"], 200>;
+type RequestType = InferRequestType<typeof client.api.workspaces[":workspaceId"]["$delete"]>;
+
+export const useDeleteWorkspace = () => {
+    const queryClient = useQueryClient();
+
+    const mutation = useMutation<ResponseType, Error, RequestType>({
+        mutationFn: async ({ param }) => {
+            const response = await client.api.workspaces[":workspaceId"]["$delete"]({param});
+
+            if(!response.ok){
+                throw new Error("Failed to Delete workspace");
+            }
+
+            return await response.json();
+        },
+        onSuccess({data}){
+            toast.success("Workspace Deleted");
+            queryClient.invalidateQueries({ queryKey: ["workspaces"]});
+            queryClient.invalidateQueries({ queryKey: ["workspace", data.$id]});
+        },
+        onError(){
+            toast.error("failed to delete workspace");
+        }
+    });
+    return mutation;
+}
---
+++ src/hooks/use-confirm.tsx
@@ -0,0 +1,62 @@
+"use client"
+
+import { useState } from "react";
+import { Button, type ButtonProps } from "@/components/ui/button";
+import { ResponsiveModal } from "@/components/responsive-modal";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+
+export const useConfirm = (
+    title: string,
+    message: string,
+    variant: ButtonProps["variant"] = "primary",
+): [ () => JSX.Element, () => Promise<unknown>] => {
+
+    const [promise, setPromise] = useState<{ resolve: (value: boolean) => void } | null>(null);
+
+    const confirm = () => { 
+        return new Promise((resolve) => {
+            setPromise({resolve});
+        });
+    }
+
+    const handleClose = () => {
+        setPromise(null);
+    }
+
+    const handleConfirm = () => {
+        promise?.resolve(true);
+        handleClose();
+    }
+
+    const handleCancel = () => {
+        promise?.resolve(false);
+        handleClose();
+    }
+
+    const ConfirmationDialog = () => (
+        <ResponsiveModal open={promise !== null} onOpenChange={handleClose}>
+            <Card className="w-full h-full shadow-none border-none">
+                <CardContent className="pt-8">
+                    <CardHeader className="p-0">
+                        <CardTitle>
+                            {title}
+                        </CardTitle>
+                        <CardDescription>
+                            {message}
+                        </CardDescription>
+                    </CardHeader>
+                    <div className="pt-4 w-full flex flex-col items-center justify-end gap-y-2 gap-x-2 sm:flex-row">
+                        <Button onClick={handleCancel} variant="outline" className="w-full sm:w-auto">
+                            Cancel
+                        </Button>
+                        <Button onClick={handleConfirm} variant={variant} className="w-full sm:w-auto">
+                            Confirm
+                        </Button>
+                    </div>
+                </CardContent>
+            </Card>
+        </ResponsiveModal>
+    );
+
+    return [ConfirmationDialog, confirm];
+};
