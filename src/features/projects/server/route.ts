3c4ef263ed38dafc9d3406e0705f89da2ac3cda3
Shujaat Khan <154977983+shujaatk92@users.noreply.github.com>
2024-11-04 12:31:13 +0000

    2-26:00

--- src/features/projects/server/route.ts
+++ src/features/projects/server/route.ts
@@ -27,7 +27,7 @@ const app = new Hono()
         });
 
         if(!member){
-            return c.json({error: "Unauthorized"});
+            return c.json({error: "Unauthorized"}, 401);
         }
 
         let uploadedImageUrl: string | undefined;
