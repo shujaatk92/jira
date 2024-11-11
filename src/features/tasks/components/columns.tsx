3ecbc0545fe090f98c9570f7aad399bf1b8f5011
Shujaat Khan <154977983+shujaatk92@users.noreply.github.com>
2024-11-11 08:37:41 +0000

    2-3:02:48

--- src/features/tasks/components/columns.tsx
+++ src/features/tasks/components/columns.tsx
@@ -116,7 +116,7 @@ export const columns: ColumnDef<Task>[] = [
         },
         cell: ({row}) => {
             const status = row.original.status;
-            return <Badge>{snakeCaseToTitleCase(status)}</Badge>
+            return <Badge variant={status}>{snakeCaseToTitleCase(status)}</Badge>
           }
     },
 ];
---
+++ src/features/tasks/components/task-actions.tsx
@@ -0,0 +1,14 @@
+
+interface TaskActionsProps {
+    id: string;
+    projectId: string;
+    children: React.ReactNode;
+}
+
+export const TaskActions = () =>{
+    return(
+        <div>
+            
+        </div>
+    )
+}
